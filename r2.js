64a5f933c558185f4a73c7f18b0df9bd55cc48e2
imobem-tech <imobem@gmail.com>
2026-04-26 02:06:45 -0300

    Update r2.js

--- r2.js
+++ r2.js
@@ -5,8 +5,8 @@ const s3 = new S3Client({
   endpoint: process.env.R2_ENDPOINT,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
-    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
-  },
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
+  }
 });
 
 export async function uploadArquivo(buffer, nomeArquivo, contentType) {
@@ -14,10 +14,14 @@ export async function uploadArquivo(buffer, nomeArquivo, contentType) {
     Bucket: process.env.R2_BUCKET,
     Key: nomeArquivo,
     Body: buffer,
-    ContentType: contentType,
+    ContentType: contentType
   });
 
   await s3.send(command);
 
-  return `${process.env.R2_ENDPOINT}/${process.env.R2_BUCKET}/${nomeArquivo}`;
+  return {
+    bucket: process.env.R2_BUCKET,
+    key: nomeArquivo,
+    urlInterna: `${process.env.R2_ENDPOINT}/${process.env.R2_BUCKET}/${nomeArquivo}`
+  };
 }
